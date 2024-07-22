5a4761886a99dd1f63d941e2a6ff8e04efdcb645
Gabriel Mathias <gablemathias@icloud.com>
2024-07-22 19:40:50 -0300

    getting legislators info from the API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,14 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = File.read('secret.key').strip
 
 puts 'Event Manager Initiliazed!'
+
+def handle_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')
+end
 
 if File.exist? 'event_attendees.csv'
   contents = CSV.open(
@@ -10,13 +18,21 @@ if File.exist? 'event_attendees.csv'
   )
 end
 
-def handle_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')
-end
-
 contents.each do |row|
   name = row[:first_name]
   zipcode = handle_zipcode(row[:zipcode])
 
-  puts "#{name} - #{zipcode}"
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+
+    legislators = legislators.officials
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} - #{zipcode} - #{legislators}"
 end
