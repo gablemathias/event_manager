a57537c3c6462d3c4b041d7fd59b1d949c84748f
Gabriel Mathias <gablemathias@icloud.com>
2024-07-22 18:55:43 -0300

    handling zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,30 @@
+require 'csv'
+
 puts 'Event Manager Initiliazed!'
+
+if File.exist? 'event_attendees.csv'
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+end
+
+def handle_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.size < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.size > 5
+    zipcode.slice(...5)
+  else
+    zipcode
+  end
+end
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = handle_zipcode(row[:zipcode])
+
+  puts "#{name} - #{zipcode}"
+end
