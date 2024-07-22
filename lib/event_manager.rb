e031278cfc5788f93cb9a56fd00982988b53c03c
Gabriel Mathias <gablemathias@icloud.com>
2024-07-22 19:59:49 -0300

    move legislators name logic to a method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,28 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = File.read('secret.key').strip
-
 puts 'Event Manager Initiliazed!'
 
 def handle_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')
+end
+
+def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+
+    legislators = legislators.officials
+    legislators.map(&:name).join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 if File.exist? 'event_attendees.csv'
@@ -21,19 +36,7 @@ end
 contents.each do |row|
   name = row[:first_name]
   zipcode = handle_zipcode(row[:zipcode])
+  legislator = legislators_by_zipcode(zipcode)
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-
-    legislators = legislators.officials
-    legislator_name = legislators.map(&:name).join(', ')
-  rescue StandardError
-    legislator_name = 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials' # rubocop:disable Layout/LineLength
-  end
-
-  puts "#{name} - #{zipcode} - #{legislator_name}"
+  puts "#{name} - #{zipcode} - #{legislator}"
 end
